766e2b882769204e79422c019b2893417a80f7e8
Christian Loos <cloos@netcologne.de>
2012-07-16 14:20:48 +0200

    new GetAllHeaders method from 4.2/attach-from-transactions branch

---
+++ lib/RT/Attachment_Vendor.pm
@@ -0,0 +1,18 @@
+use strict;
+no warnings;
+
+package RT::Attachment;
+
+# NOTE: new GetAllHeaders method from 4.2/attach-from-transactions branch
+sub GetAllHeaders {
+    my $self = shift;
+    my $tag = shift;
+    my @values = ();
+    foreach my $line ($self->_SplitHeaders) {
+        next unless $line =~ /^\Q$tag\E:\s+(.*)$/si;
+        push @values, $1;
+    }
+    return @values;
+}
+
+1;
