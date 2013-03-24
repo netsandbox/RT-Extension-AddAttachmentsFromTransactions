66af2de8ac33016bdaf9d778897b1416cf7599e5
Christian Loos <cloos@netcologne.de>
2013-03-24 13:50:52 +0100

    don't require Hook::LexWrap anymore

--- lib/RT/Extension/AddAttachmentsFromTransactions.pm
+++ lib/RT/Extension/AddAttachmentsFromTransactions.pm
@@ -3,8 +3,27 @@ package RT::Extension::AddAttachmentsFromTransactions;
 use 5.008003;
 use strict;
 use warnings;
+no warnings 'redefine';
 
 our $VERSION = '0.01';
+
+use RT::Ticket;
+my $orig_note = RT::Ticket->can('_RecordNote');
+*RT::Ticket::_RecordNote = sub {
+    my $self = shift;
+    my %args = @_;
+
+    # We can't do anything if we don't have an MIMEObj
+    # so let the original method handle it
+    return $orig_note->($self, %args) unless $args{'MIMEObj'};
+
+    # move the Attachment id's from session to the RT-Attach header
+    for my $id ( @{ $HTML::Mason::Commands::session{'AttachExisting'} } ) {
+        $args{'MIMEObj'}->head->add( 'RT-Attach' => $id );
+    }
+
+    return $orig_note->($self, %args);
+};
 
 =encoding utf8
 
@@ -75,18 +94,5 @@ RT 4.2 on the '4.2/attach-from-transactions' branch where I've borrowed some
 code for this extension.
 
 =cut
-
-use Hook::LexWrap;
-
-use RT::Ticket;
-wrap 'RT::Ticket::_RecordNote',
-    pre => sub{
-        my %args = @_[1..@_-2];
-
-        # move the Attachment id's from session to the RT-Attach header
-        for my $id ( @{ $HTML::Mason::Commands::session{'AttachExisting'} } ) {
-            $args{'MIMEObj'}->head->add( 'RT-Attach' => $id );
-        }
-    };
 
 1;
